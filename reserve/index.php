b7b4b31d9e11db99d18b5c564f377fd404a83fce
drixton <drixdiocton@gmail.com>
2024-05-09 17:40:57 +0800

    udpate

--- reserve/index.php
+++ reserve/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
     <link rel="icon" href="/Reservation_System/reserve/assets/icons/titleicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/landpage.css">     
     <style>
             body {
     background-color: #040F13; /* Background color */
