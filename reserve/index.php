26f896f1ccb46be89d897df0d5cd5278082ea627
Your Name <you@example.com>
2024-05-14 12:48:15 +0800

    dashboard update

--- reserve/index.php
+++ reserve/index.php
@@ -7,14 +7,39 @@
     <link rel="icon" href="/Reservation_System/reserve/assets/icons/titleicon.png" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/landpage.css">     
     <style>
-            body {
-    background-color: #040F13; /* Background color */
-}
+        body {
+            background-color: #040F13; /* Background color */
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+        }
+        
+        main {
+            flex: 1;
+        }
+
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
 
     </style>
 </head>
 <body>
 <?php include 'headbar.php'; ?> 
-   
+
+<main>
+    <!-- Your main content goes here -->
+</main>
+
+<?php include 'footer.php'; ?>
+
 </body>
 </html>
