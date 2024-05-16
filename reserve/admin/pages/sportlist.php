6b7e9181c0ceaf7ec6989dd4a88a8c780d012058
Your Name <you@example.com>
2024-05-16 14:25:32 +0800

    update

--- reserve/admin/pages/sportlist.php
+++ reserve/admin/pages/sportlist.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Units Report List</title>
+    <link rel="icon" href="/Reservation_System/reserve/assets/icons/titleicon.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
